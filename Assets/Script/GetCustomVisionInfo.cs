7b857beafc76f9ad19c7d73d88d7348b6f4ea358
DESKTOP-34JNQ2S\smori <mori1979shingo@gmail.com>
2018-05-18 17:35:50 +0900

    first commit

---
+++ Assets/Script/GetCustomVisionInfo.cs
@@ -0,0 +1,176 @@
+﻿// Copyright(c) 2018 Shingo Mori
+// Released under the MIT license
+// http://opensource.org/licenses/mit-license.php
+
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VR.WSA.WebCam;
+
+public class GetCustomVisionInfo : MonoBehaviour, IInputClickHandler
+{
+    // UI周りのフィールド
+    public Text SystemMessage;       //状況表示用メッセージエリア
+    public RawImage photoPanel;      //debug用のキャプチャ表示パネル
+
+    // カメラ周りのパラメータ
+    private PhotoCapture photoCaptureObject = null;
+    private Resolution cameraResolution;
+    private Quaternion cameraRotation;
+
+    // Azure側のパラメータ群
+    private string visionAPIKey = "YOUR_APP_KEY"; //Custom VisionのAPPキーをセットする
+    private string visionURL    = "YOUR_APP_URL"; //Custom VisionのURLをセットする
+
+    private TextToSpeech tts;
+    private float probabilityThreshold = 0.5f; //信頼度の閾値
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        AnalyzeScene();
+    }
+
+    void Start()
+    {
+        InputManager.Instance.AddGlobalListener(gameObject);
+        cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        tts = gameObject.GetComponent<TextToSpeech>();
+    }
+
+    private void AnalyzeScene()
+    {
+        DisplaySystemMessage("Detect Start...");
+        PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
+    }
+
+    //PhotoCaptureの取得は下記参照
+    //https://docs.microsoft.com/ja-jp/windows/mixed-reality/locatable-camera-in-unity
+    private void OnPhotoCaptureCreated(PhotoCapture captureObject)
+    {
+        DisplaySystemMessage("Take Picture...");
+        photoCaptureObject = captureObject;
+
+        CameraParameters c = new CameraParameters();
+        c.hologramOpacity = 0.0f;
+        c.cameraResolutionWidth = cameraResolution.width;
+        c.cameraResolutionHeight = cameraResolution.height;
+        c.pixelFormat = CapturePixelFormat.JPEG;
+
+        captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
+    }
+
+    private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
+    {
+        if (result.success)
+        {
+            photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+        }
+        else
+        {
+            Debug.LogError("Unable to start photo mode!");
+            throw new Exception();
+        }
+    }
+
+    private void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        if (result.success)
+        {
+            // Custom Visionに送るimageBufferListにメモリ上の画像をコピーする
+            List<byte> imageBufferList = new List<byte>();
+            photoCaptureFrame.CopyRawImageDataIntoBuffer(imageBufferList);
+
+            //ここはデバッグ用 送信画像の出力。どんな画像が取れたのか確認したい場合に使用。邪魔ならphotoPanelごと消してもよい。
+            Texture2D debugTexture = new Texture2D(100, 100);
+            debugTexture.LoadImage(imageBufferList.ToArray());
+            photoPanel.texture = debugTexture;
+
+            StartCoroutine(PostToCustomVisionAPI(imageBufferList.ToArray()));
+        }
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+    }
+
+    private void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+
+    /*
+     * 取得した画像をCustom Vision APIに送信し、タグ、キャプションを取得する
+     */
+    private IEnumerator<object> PostToCustomVisionAPI(byte[] imageData)
+    {
+        DisplaySystemMessage("Call Custom Vision API...");
+        var headers = new Dictionary<string, string>() {
+            { "Prediction-Key", visionAPIKey },
+            { "Content-Type", "application/octet-stream" }
+        };
+
+        WWW www = new WWW(visionURL, imageData, headers);
+        yield return www;
+
+        ResponceJson json = JsonUtility.FromJson<ResponceJson>(www.text);
+
+        float tmpProbability = 0.0f;
+        string str = "";
+
+        // probabilityの高い順に格納されているが、念のため全量取得してログ出力
+        for (int i = 0; i < json.predictions.Length; i++)
+        {
+            Prediction obj = (Prediction)json.predictions[i];
+
+            Debug.Log(obj.tagName + "：" + obj.probability.ToString("P"));
+
+            if (tmpProbability < obj.probability)
+            {
+                str = obj.probability.ToString("P") + "の確率で" + obj.tagName + "です";
+                str = "It's a " + obj.tagName + "! The Probability is " + obj.probability.ToString("P");
+                tmpProbability = obj.probability;
+            }
+
+            // probabilityが閾値未満だったら特定不能とする
+            if(tmpProbability < probabilityThreshold)
+            {
+                str = "Oops! I couldn't identify it. Please Try Again...";
+            }
+        }
+
+        DisplaySystemMessage(str);
+        tts.StartSpeaking(str);
+    }
+
+    /*
+     * 状況出力用メッセージ
+     */
+    private void DisplaySystemMessage(string message)
+    {
+        SystemMessage.text = message;
+    }
+
+    /*
+     * ここから、Custom Vision APIの戻り値用クラス
+     */
+    [Serializable]
+    private class ResponceJson
+    {
+        public string id;
+        public string project;
+        public string iteration;
+        public string created;
+
+        public Prediction[] predictions;
+    }
+
+    [Serializable]
+    private class Prediction
+    {
+        public float probability;
+        public string tagId;
+        public string tagName;
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# HoloLensCustomVisionSample
+
+de:code 2018 AC62「簡単！！HoloLensで始めるCognitive Services～de:code 2018特別バージョン～」の  
+Custom Vision用サンプルコードです。  
+HoloLensで画像キャプチャを取得し、Custom Visionを呼び出すことで\
+対象物を分類分けして表示、音声出力します。
+
+![image](https://github.com/haveagit/HoloLensCustomVisionAPISample/tree/master/Assets/Image/cat.jpg)
+
+信頼度が50%（※）を切る場合は「判定不能」としますので、
+その場合は撮影しなおしてください。
+※閾値はプログラム内で変更可能
+
+## バージョン情報
+ Unity：2017.1.2p3  
+ MRToolkit：HoloToolkit-Unity-v1.2017.1.2  
+ VisualStudio：15.5.4  
+
+## 使い方
+
+1.本PJをクローンし、Azure Custom Vision APIのimage fileのキーを  
+ GetCustomVisionInfo.cs の visionAPIKeyに設定してください。  
+
+2. 1と同じように、visionURLにCustom Visionのimage fileのURLを設定してください。
+
+3.エアタップで画像取得～Custom Visionの呼び出しを行います。  
+
+# 注意点
+
+1.AzureおよびCustom Vision 自体の操作、設定に関しては本PJ内では説明致しません。
+  Custom Visionについては下記の記事も参考になります。  
+  情報は2017年のものなので古いですが、Custom Visionの設定周りはほぼ変わりませんでした（2018年5月時点）。
+ ![HoloLensで始めるCognitive Services（Custom Vision Services編）](https://qiita.com/morio36/items/42ee34a1c97929d44ca2)
